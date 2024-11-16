e66798af9821c958fea651a2bc2aea0b946fa116
Paulo Ricardo M <pr741234@gmail.com>
2024-11-16 20:37:36 -0300

    chore: Configuring swagger with keycloak

--- src/Orange.API/Program.cs
+++ src/Orange.API/Program.cs
@@ -1,8 +1,43 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(config =>
+	{
+		config.SwaggerDoc("v1", new OpenApiInfo
+		{
+			Title = "Orange API",
+			Version = "v1",
+			Description = "Orange API Documentation"
+		});
+
+		config.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+		{
+			Description = "JWT Authorization header using the Bearer scheme.",
+			Name = "Authorization",
+			In = ParameterLocation.Header,
+			Type = SecuritySchemeType.Http,
+			Scheme = "bearer",
+			BearerFormat = "JWT"
+		});
+
+		config.AddSecurityRequirement(new OpenApiSecurityRequirement
+			{
+				{
+					new OpenApiSecurityScheme
+					{
+						Reference = new OpenApiReference
+						{
+							Type = ReferenceType.SecurityScheme,
+							Id = "Bearer"
+						}
+					},
+					Array.Empty<string>()
+				}
+			});
+	});
 
 var app = builder.Build();
 
