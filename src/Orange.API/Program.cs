14d17da3fec1c980b5c6c386d69ecb0a47a2a758
Paulo Ricardo M <pr741234@gmail.com>
2024-11-17 20:01:55 -0300

    chore: Configure Authentication Setup

--- src/Orange.API/Program.cs
+++ src/Orange.API/Program.cs
@@ -1,43 +1,11 @@
-using Microsoft.OpenApi.Models;
+using Orange.API.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(config =>
-	{
-		config.SwaggerDoc("v1", new OpenApiInfo
-		{
-			Title = "Orange API",
-			Version = "v1",
-			Description = "Orange API Documentation"
-		});
-
-		config.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-		{
-			Description = "JWT Authorization header using the Bearer scheme.",
-			Name = "Authorization",
-			In = ParameterLocation.Header,
-			Type = SecuritySchemeType.Http,
-			Scheme = "bearer",
-			BearerFormat = "JWT"
-		});
-
-		config.AddSecurityRequirement(new OpenApiSecurityRequirement
-			{
-				{
-					new OpenApiSecurityScheme
-					{
-						Reference = new OpenApiReference
-						{
-							Type = ReferenceType.SecurityScheme,
-							Id = "Bearer"
-						}
-					},
-					Array.Empty<string>()
-				}
-			});
-	});
+builder.Services.AddSwaggerSetup();
+builder.Services.AddAuthenticationSetup();
 
 var app = builder.Build();
 
@@ -48,6 +16,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
