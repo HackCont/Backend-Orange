0ee6f9c3c59fe73b86075dcc3a37ef7d510ef366
Paulo Ricardo M <pr741234@gmail.com>
2024-11-19 00:22:32 -0300

    feat: Extension method for Migrations
    - Configure migration execution on app startup

---
+++ src/Orange.API/Extensions/MigrationExtensions.cs
@@ -0,0 +1,27 @@
+namespace Orange.API.Extensions;
+
+using Microsoft.EntityFrameworkCore;
+using Orange.API.Data;
+
+public static class MigrationExtensions
+{
+	public static IHost MigrateDatabase(this IHost host)
+	{
+		using (var scope = host.Services.CreateScope())
+		{
+			var services = scope.ServiceProvider;
+
+			try
+			{
+				var context = services.GetRequiredService<AppDbContext>();
+				context.Database.Migrate();
+			}
+			catch (Exception ex)
+			{
+
+				throw new InvalidOperationException("An error occurred while migrating the database.", ex);
+			}
+		}
+		return host;
+	}
+}
