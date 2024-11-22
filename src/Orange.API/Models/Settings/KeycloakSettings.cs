d4e5955d22f1e93ecdc6685a754ff3f6fa7394bc
Paulo Ricardo M <pr741234@gmail.com>
2024-11-22 12:43:01 -0300

    feat: Configure keycloak settings model

--- src/Orange.API/Models/Settings/KeycloakSettings.cs
+++ src/Orange.API/Models/Settings/KeycloakSettings.cs
@@ -5,4 +5,13 @@ public class KeycloakSettings
 	public required string Authority { get; set; }
 	public required string Audience { get; set; }
 	public required bool RequireHttpsMetadata { get; set; }
+	public required string ClientId { get; set; }
+	public required string ClientSecret { get; set; }
+	public required KeycloakUri Uri { get; set; }
+}
+
+public class KeycloakUri
+{
+	public required string Register { get; set; }
+	public required string Login { get; set; }
 }
